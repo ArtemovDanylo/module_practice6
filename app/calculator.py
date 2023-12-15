bb61d488dc23ca69a7a8c564b576f6f804a7d9f0
Danylo <artdanier@gmail.com>
2023-12-15 15:06:07 +0200

    Created class Calculator

---
+++ app/calculator.py
@@ -0,0 +1,29 @@
+import random
+import math
+
+
+class Calculator():
+    def __init__(self, number1, number2):
+        self.number1 = number1
+        self.number2 = number2
+
+    def addition(self):
+        return self.number1 + self.number2
+
+    def subtraction(self):
+        return self.number1 - self.number2
+
+    def multiplication(self):
+        return self.number1 * self.number2
+
+    def division(self):
+        if self.number2 != 0:
+            return self.number1 / self.number2
+        else:
+            print("Division be zero is not allowed")
+
+    def pow(self):
+        return math.pow(self.number1, self.number2)
+
+    def static_random_generate(a, b):
+        return random.randint(a, b)
---
+++ main.py
@@ -0,0 +1,8 @@
+from app.calculator import Calculator
+
+
+def main():
+    my_calculator = Calculator
+
+
+main()
